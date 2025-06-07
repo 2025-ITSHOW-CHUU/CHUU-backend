1d020b2f9530b024f1d33f4a9766924945a1a1d0
subin1848 <s2315@e-mirim.hs.kr>
2025-06-07 12:00:12 +0900

    ✨ [feat] 채팅 생성 및 조회 기능 구현

---
+++ src/gemini/chatbot/chat.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ChatController } from "./chat.controller";
+import { ChatService } from "./chat.service";
+import { Chat, ChatSchema } from "./schemas/chat.schema";
+import { TeacherModule } from "./teacher.module";
+import { GeminiService } from "../application/gemini.service";
+import { GeminiModule } from "../gemini.module";
+import { Teacher, TeacherSchema } from "./schemas/teacher.schema";
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema },
+                                    { name: Teacher.name, schema: TeacherSchema }
+        ]),
+        TeacherModule,
+        GeminiModule,
+    ],
+    controllers: [ChatController],
+    providers: [ChatService, GeminiService],
+    exports: [ChatService],
+})
+export class ChatModule {}
