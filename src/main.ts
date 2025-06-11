e2455cf2b936b85abc404ebf17a8552e4d34dfca
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-06-12 08:14:17 +0900

    https 세팅

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,21 @@ import * as cors from 'cors';
 import * as express from 'express';
 import { join } from 'path';
 import * as bodyParser from 'body-parser';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(
+      '/etc/letsencrypt/live/chuu.mirim-it-show.site/privkey.pem',
+    ),
+    cert: fs.readFileSync(
+      '/etc/letsencrypt/live/chuu.mirim-it-show.site/fullchain.pem',
+    ),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
 
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
