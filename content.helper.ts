656ec6b44894f8be5a6c659b68e60ccd818a3069
subin1848 <s2315@e-mirim.hs.kr>
2025-06-13 11:44:46 +0900

    ✨ [feat] 메세지 content 변환

--- content.helper.ts
+++ content.helper.ts
@@ -1,14 +1,12 @@
 import { Content } from "@google/generative-ai";
 
-export function createContent(text: string): Content[] {
-    return [
-        {
-            role: 'user',
-            parts: [
-                {
-                    text,
-                },
-            ],
-        },
-    ];
+export function createContent(messages: { role: 'user' | 'assistant'; content: string }[]): Content[] {
+    return messages.map(msg => ({
+        role: msg.role,
+        parts: [
+            {
+                text: msg.content
+            }
+        ]
+    }))
 }
