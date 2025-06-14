d40b6c838e5464bfed7aaca3c67638be6813b5db
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-06-14 16:58:18 +0900

    id 수정

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -51,12 +51,12 @@ export class UserService implements OnModuleInit {
 
   async getMaxUser() {
     const teachers = [
-      { id: '조예진', teacherScore: 0 },
-      { id: '장하나', teacherScore: 0 },
-      { id: '박지우', teacherScore: 0 },
-      { id: '김윤지', teacherScore: 0 },
-      { id: '이호연', teacherScore: 0 },
-      { id: '이대형', teacherScore: 0 },
+      { id: '조예진 선생님', teacherScore: 0 },
+      { id: '장하나 선생님', teacherScore: 0 },
+      { id: '박지우 선생님', teacherScore: 0 },
+      { id: '김윤지 선생님', teacherScore: 0 },
+      { id: '이호연 선생님', teacherScore: 0 },
+      { id: '이대형 선생님', teacherScore: 0 },
     ];
 
     for (const teacherObject of teachers) {
